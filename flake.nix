d058abc07336f17434c1f1380cd8ed08e827007b
Dylan R. Johnston <dylan.johnston@familyzone.com>
2022-07-19 11:38:34 +0800

    Build web-app with nix too

--- flake.nix
+++ flake.nix
@@ -11,53 +11,62 @@
     nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter, naersk, fenix }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      cross-toolchain = with fenix.packages.${system}; combine [
-        minimal.cargo
-        minimal.rustc
-        targets.wasm32-unknown-unknown.latest.rust-std
-      ];
-      cross-compile = naersk.lib.${system}.override {
-        cargo = cross-toolchain;
-        rustc = cross-toolchain;
-      };
-      dev-toolchain = fenix.packages.${system}.complete.withComponents [
-        "cargo"
-        "rustc"
-        "rustfmt"
-        "clippy"
-        "rust-src"
-      ];
-      darwin-support =
-        if pkgs.stdenv.isDarwin then with pkgs.darwin.apple_sdk.frameworks; [ Security AppKit ] else [ ];
-    in
-    {
-      defaultPackage = cross-compile.buildPackage {
-        src = nix-filter.lib {
-          root = ./.;
-          include = [
-            "Cargo.lock"
-            "Cargo.toml"
-            (nix-filter.lib.inDirectory "src")
-          ];
+  outputs = { self, nixpkgs, flake-utils, nix-filter, naersk, fenix }: flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        wasm-toolchain = with fenix.packages.${system}; combine [
+          minimal.cargo
+          minimal.rustc
+          targets.wasm32-unknown-unknown.latest.rust-std
+        ];
+        rust-wasm = naersk.lib.${system}.override {
+          cargo = wasm-toolchain;
+          rustc = wasm-toolchain;
         };
-        CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
-      };
-      devShell = pkgs.mkShell {
-        buildInputs =
-          [
-            dev-toolchain
-            pkgs.libiconv
-            pkgs.sqlite
-            pkgs.lldb
-            pkgs.wasm-bindgen-cli
-            pkgs.wasm-pack
-            pkgs.simple-http-server
-          ] ++ darwin-support;
+        dev-toolchain = fenix.packages.${system}.complete.withComponents [
+          "cargo"
+          "rustc"
+          "rustfmt"
+          "clippy"
+          "rust-src"
+        ];
+        darwin-support =
+          if pkgs.stdenv.isDarwin then with pkgs.darwin.apple_sdk.frameworks; [ Security AppKit ] else [ ];
+      in
+      rec {
+        packages = {
+          wasm = rust-wasm.buildPackage {
+            CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+            src = nix-filter.lib {
+              root = ./.;
+              include = [
+                "Cargo.lock"
+                "Cargo.toml"
+                (nix-filter.lib.inDirectory "src")
+              ];
+            };
+          };
+          web-app = pkgs.runCommand "roguelike-web-app" { } ''
+            ${pkgs.wasm-bindgen-cli}/bin/wasm-bindgen ${packages.wasm}/bin/roguelike.wasm --out-dir $out/wasm --no-modules --no-typescript
+            cp ${./public/index.html} $out/index.html
+          '';
+        };
+        defaultPackage = packages.web-app;
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [
+              dev-toolchain
+              pkgs.libiconv
+              pkgs.sqlite
+              pkgs.lldb
+              pkgs.wasm-bindgen-cli
+              pkgs.wasm-pack
+              pkgs.simple-http-server
+            ] ++ darwin-support;
 
-        RUST_SRC_PATH = "${dev-toolchain}/lib/rustlib/src/rust/library";
-      };
-    });
+          RUST_SRC_PATH = "${dev-toolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
 }
